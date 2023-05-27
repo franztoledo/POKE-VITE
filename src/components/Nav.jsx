e9b66d629c8cf9773adb9784d9b52877834aaf8d
Franz Toledo <franz.wilber.tm@gmail.com>
2023-05-27 18:01:01 -0500

    NavComponent-Styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
-import { Outlet } from 'react-router'
+import { Link, Outlet  } from 'react-router-dom'
+import './Nav.css'
+import { PokeballIconSmall } from '../assets/icon'
 
 function Nav() {
   return (
     <div>
-        <h1>
-            Navegatcion
-        </h1>
+        <header className='container'>
+			<Link to='/' className='logo'>
+				<PokeballIconSmall/>
+                <span>
+                    Pokedex
+                </span>
+			</Link>
+
+			<form >
+				<div className='form-group'>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						fill='none'
+						viewBox='0 0 24 24'
+						strokeWidth='1.5'
+						stroke='currentColor'
+						className='icon-search'
+					>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
+						/>
+					</svg>
+					<input
+						type='search'
+						name='valueSearch'
+						placeholder='Buscar nombre de pokemon'
+					/>
+				</div>
+
+				<button className='btn-search'>Buscar</button>
+			</form>
+		</header>
         <Outlet/>
     </div>
   )
