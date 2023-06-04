73eb0b60e36ac2f946b129a7af2a2e8a78ac13a1
Franz Toledo <franz.wilber.tm@gmail.com>
2023-06-03 20:28:28 -0500

    Favorite component, context, useLocalStorage

--- src/components/CardPokemon/CardPokemon.jsx
+++ src/components/CardPokemon/CardPokemon.jsx
@@ -5,24 +5,26 @@ import { capitalize } from '../../helper/helper'
 
 function CardPokemon({pokemon}) {
     return (
-        <Link to={`/pokemon/${pokemon.id}`} className={`card-pokemon ${pokemon.types[0].type.name}`}>
-            <div className='card-img'>
-				<img
-					src={pokemon.sprites.other.home.front_default}
-					alt={`Pokemon ${pokemon.name}`}
-				/>
-			</div>
-			<div className='card-info'>
-				<h3>{capitalize(pokemon.name)}</h3>
-				<div className='card-types'>
-					{pokemon.types.map(type => (
-						<span key={type.type.name} className={type.type.name}>
-							{type.type.name}
-						</span>
-					))}
+		<div className={`card-pokemon ${pokemon.types[0].type.name}`}>
+			<Link to={`/pokemon/${pokemon.id}`} className={`card-pokemon ${pokemon.types[0].type.name}`}>
+				<div className='card-img'>
+					<img
+						src={pokemon.sprites.other.home.front_default}
+						alt={`Pokemon ${pokemon.name}`}
+					/>
 				</div>
-			</div>
-        </Link>
+				<div className='card-info'>
+					<h3>{capitalize(pokemon.name)}</h3>
+					<div className='card-types'>
+						{pokemon.types.map(type => (
+							<span key={type.type.name} className={type.type.name}>
+								{type.type.name}
+							</span>
+						))}
+					</div>
+				</div>
+			</Link>
+		</div>
     )
 }
 
